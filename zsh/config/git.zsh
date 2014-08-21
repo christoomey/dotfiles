a0087850af39e059c526d55fcb3d22e908cb5fb1
Chris Toomey <chris@ctoomey.com>
2014-08-21 14:49:35 -0400

    Extract git functions to their own zsh config file

---
+++ zsh/config/git.zsh
@@ -0,0 +1,22 @@
+compdef g=git
+function g {
+  if [[ $# > 0 ]]; then
+    if [ $1 = "." ]; then
+      git status --short --branch .
+    else
+      git $@
+    fi
+  else
+    if behind_master; then yellow_msg 'behind master'; fi
+    # behind upstream warning
+    git status --short --branch
+  fi
+}
+
+function yellow_msg {
+  echo "\033[33m$1\033[0m"
+}
+
+function behind_master {
+  test $(git merge-base HEAD master) != $(git rev-parse master)
+}
