d5b6da08780626c60a16a812058d77e810c91b73
Chris Toomey <chris@ctoomey.com>
2014-12-09 17:03:39 -0500

    Add alfred dir to cdpath

--- zsh/config/navigation.zsh
+++ zsh/config/navigation.zsh
@@ -1,3 +1,3 @@
 unsetopt auto_cd # with cdpath enabled, auto_cd gives too many false positives
-cdpath=($HOME/code $HOME/code/work $HOME/code/work/current $HOME/code/vim $HOME)
+cdpath=($HOME/code $HOME/code/work $HOME/code/work/current $HOME/code/vim $HOME/code/alfred $HOME)
 alias c=cd
