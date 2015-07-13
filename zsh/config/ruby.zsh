c0b4e346e0282d19dd872a19a54941ecc5e70b5f
Chris Toomey <chris@ctoomey.com>
2015-07-13 11:43:23 -0400

    Move path & chruby config to zshenv
    zshenv is loaded by all shells, whereas zshrc is only loaded by interactive
    shells. This means Alfred workflows, Vim, sytnastic, etc will not have access
    to many things, e.g. chruby ruby version.

--- zsh/config/ruby.zsh
+++ zsh/config/ruby.zsh
@@ -1,11 +1,3 @@
-if [[ -e /usr/local/share/chruby ]]; then
-  source '/usr/local/share/chruby/chruby.sh'
-  source '/usr/local/share/chruby/auto.sh'
-  if [[ -f ~/.ruby-version ]]; then
-    chruby $(cat ~/.ruby-version)
-  fi
-fi
-
 function rbinstall() {
   command ruby-install ruby $(cat .ruby-version) > /dev/null
 }
