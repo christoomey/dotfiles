e952e847d50f8a4d286fe7ed518292dd62007cdf
Chris Toomey <chris@ctoomey.com>
2015-08-18 11:03:30 -0400

    Simplify g git wrapper

--- zsh/config/git.zsh
+++ zsh/config/git.zsh
@@ -1,22 +1,16 @@
 compdef g=git
 function g {
   if [[ $# -gt 0 ]]; then
-    if [ $1 = "." ]; then
-      git status --short --branch .
-    else
-      git "$@"
-    fi
+    git "$@"
   else
-    if behind_master; then yellow_msg 'behind master'; fi
-    # behind upstream warning
     git status --short --branch
   fi
 }
 
-function yellow_msg {
-  echo "\033[33m$1\033[0m"
-}
-
-function behind_master {
-  test $(git merge-base HEAD master) != $(git rev-parse master)
+function gitup {
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+        cd "./$(git rev-parse --show-cdup)"
+    else
+        return
+    fi
 }
