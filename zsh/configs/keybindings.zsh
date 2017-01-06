8858cfbf16a35029c31de170ab1625c2dcd4dd07
Chris Toomey <chris@ctoomey.com>
2017-01-06 12:48:12 -0500

    zsh: Extract keybindings and enhance zle widgets

---
+++ zsh/configs/keybindings.zsh
@@ -0,0 +1,38 @@
+# handy keybindings
+bindkey "^a" beginning-of-line
+bindkey "^e" end-of-line
+bindkey "^f" forward-char
+bindkey "^b" backward-char
+bindkey "^k" kill-line
+bindkey "^d" delete-char
+bindkey "^p" history-search-backward
+bindkey "^n" history-search-forward
+bindkey "^y" accept-and-hold
+bindkey "^w" backward-kill-word
+bindkey "^u" backward-kill-line
+
+# Open current command in Vim
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^x^e" edit-command-line
+
+# Copy the most recent command to the clipboard
+function _pbcopy_last_command(){
+  history | tail -1 | sed 's/ *[0-9]* *//' | pbcopy
+}
+zle -N pbcopy-last-command _pbcopy_last_command
+bindkey '^x^y' pbcopy-last-command
+
+# Fuzzy match against history, edit selected value
+_uniqe_without_sort() { awk '!x[$0]++' }
+_fuzzy_history() {
+  zle -U "$(
+    history | \
+    tail -2000 | \
+    sed 's/ *[0-9]* *//' | \
+    _uniqe_without_sort | \
+    fzf-tmux --tac --reverse --no-sort
+  )"
+}
+zle -N fuzzy-history _fuzzy_history
+bindkey '^r' fuzzy-history
--- zshrc
+++ zshrc
@@ -7,23 +7,6 @@ zplug 'zsh-users/zsh-completions', nice:10
 
 zplug load
 
-# handy keybindings
-bindkey "^a" beginning-of-line
-bindkey "^e" end-of-line
-bindkey "^f" forward-char
-bindkey "^b" backward-char 
-bindkey "^k" kill-line
-bindkey "^d" delete-char
-bindkey "^r" history-incremental-search-backward
-bindkey "^p" history-search-backward
-bindkey "^n" history-search-forward
-bindkey "^y" accept-and-hold
-bindkey "^q" push-line-or-edit
-
-autoload -z edit-command-line
-zle -N edit-command-line
-bindkey "^x^e" edit-command-line
-
 export EDITOR="vim"
 
 for zsh_source in $HOME/.zsh/configs/*.zsh; do
@@ -36,12 +19,5 @@ colors
 
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
-
-function pbcopy-buffer(){
-  print -rn $BUFFER | pbcopy
-  zle -M "  pbcopy-ed: ${BUFFER}"
-}
-zle -N pbcopy-buffer
-bindkey '^x^y' pbcopy-buffer
 
 sz() { source ~/.zshrc }
