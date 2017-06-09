9f54651cd2da900576b1b8bbd5d40470e5a86618
Chris Toomey <chris@ctoomey.com>
2017-06-09 12:51:42 -0400

    zsh: Add awk based `first`, `second`, `last` aliases

--- zsh/configs/general.zsh
+++ zsh/configs/general.zsh
@@ -3,3 +3,18 @@
 export EDITOR="vim"
 
 sz() { source ~/.zshrc }
+
+
+first() { awk '{print $1}' }
+second() { awk '{print $2}' }
+sum() { paste -sd+ - | bc }
+
+igrep() { grep -i $@ }
+
+restart-postgres() {
+  rm /usr/local/var/postgres/postmaster.pid && ( \
+    cd ~/Library/LaunchAgents && \
+      launchctl unload homebrew.mxcl.postgresql.plist && \
+      launchctl load -w homebrew.mxcl.postgresql.plist \
+  )
+}
