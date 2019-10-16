f4db39c49b9895a7df00bf93830ce0f587bac7e5
Chris Toomey <chris@ctoomey.com>
2019-10-15 21:55:25 -0400

    Add support for git aliases in wvim

--- zsh/configs/wvim.zsh
+++ zsh/configs/wvim.zsh
@@ -4,6 +4,7 @@ wvim() {
     "alias") _edit_shell_alias "$command_name";;
     "function") _edit_shell_function "$command_name";;
     "script") _edit_script "$command_name";;
+    "git-alias") _edit_git_alias "$command_name";;
     "not found") _handle_unknown_command "$command_name"
   esac
 }
@@ -22,9 +23,30 @@ _definition_type() {
     echo "function"
   elif which "$1" > /dev/null 2>&1; then
     echo "script"
+  elif _is_git_alias "$1"; then
+    echo "git-alias"
   else
     echo "not found"
   fi
+}
+
+_is_git_alias() {
+  echo "$1" | grep -q 'git-.*' && _git_aliases | grep -qF $(_git_alias_name "$1")
+}
+
+_git_alias_name() {
+  echo "$1" | sed 's/^git-//'
+}
+
+_git_aliases() {
+  git config --get-regexp ^alias\. |\
+    sed -e s/^alias\.// -e s/\ /\ =\ / |\
+    awk '{print $1}'
+}
+
+_edit_git_alias() {
+  line=$(grep -En "^\s+$(_git_alias_name "$1") =" ~/.gitconfig | cut -d ":" -f 1)
+  vim ~/.gitconfig "+$line"
 }
 
 _edit_script() {
