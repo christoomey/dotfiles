c3778d536b2fa0263ef21babefb3c49e7b7d68ee
Chris Toomey <chris@ctoomey.com>
2024-03-04 19:11:47 -0500

    More mappings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,15 +4,39 @@ local hyper = require("hyper")
 hyper.bindAll({
   ["\\"] = fn.openRaycastExtension("raycast/clipboard-history/clipboard-history"),
   ["]"] = fn.openRaycastExtension("raycast/snippets/search-snippets"),
-  ["1"] = { base = fn.openApp("1Password"), withShift = fn.sendKeys({ "ctrl", "alt", "cmd" }, "1") },
+  ["1"] = {
+    fn.openApp("1Password"),
+    { ["shift"] = fn.sendKeys({ "ctrl", "alt", "cmd" }, "1") },
+  },
+  ["."] = {
+    fn.runShortcut("Start Session"),
+    { ["shift"] = fn.openRaycastExtension("jameslyons/session/session-finish") },
+  },
   ["space"] = fn.openApp("Finder"),
+  b = fn.sendKeys({ "shift", "ctrl", "cmd" }, "y"), -- Bartender quick search
+  c = {
+    fn.openTab("calendar.google.com/calendar/u/0/r"),
+    { ["shift"] = fn.openTab("calendar.google.com/calendar/u/1/r") },
+  },
+  e = {
+    fn.sendKeys({ "shift", "ctrl", "cmd" }, "e"), -- Expose
+    { ["shift"] = fn.sendKeys({ "shift", "ctrl", "cmd" }, "d") }, -- Desktop
+  },
   f = fn.openApp("Slack"),
+  g = {
+    fn.openTab("github.com"),
+    { ["shift"] = fn.openRaycastExtension("raycast/github/search-repositories") },
+  },
   h = {
     fn.openApp("Hammerspoon"),
     { ["shift"] = reloadHammerspoonConfig },
   },
-  i = fn.openApp("Google Chrome"),
+  i = {
+    fn.openApp("Google Chrome"),
+    { ["shift"] = fn.openRaycastExtension("Codely/google-chrome/search-bookmarks") },
+  },
   j = fn.openApp("IntelliJ IDEA"),
+  k = fn.openApp("Slack"),
   l = {
     fn.openApp("Linear"),
     { ["shift"] = fn.openRaycastExtension("thomaslombart/linear/assigned-issues") },
@@ -43,6 +67,11 @@ hyper.bindAll({
   u = {
     fn.openApp("Things3"),
     { ["shift"] = fn.sendKeys({ "cmd", "shift", "ctrl" }, "u") },
+    { ["shift,ctrl"] = fn.openRaycastExtension("loris/things/show-today-list") },
+  },
+  w = { -- Notion ("w" for "wiki")
+    fn.openTab("notion.so"),
+    { ["shift"] = fn.openRaycastExtension("notion/notion/search-page") }
   },
 })
 
