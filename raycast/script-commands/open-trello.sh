0b429df8c6ebf312b056bed71429f6d436b9040e
Chris Toomey <chris@ctoomey.com>
2024-03-26 18:32:52 -0400

    Add trello helper script

---
+++ raycast/script-commands/open-trello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Trello
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon icons/trello-logo.png
+
+# Documentation:
+# @raycast.author Chris Toomey
+# @raycast.authorURL https://ctoomey.com
+
+~/bin/open-tab 'https://trello.com/'
