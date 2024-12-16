c6aec07319d14b778aee3354c747b537d71bb14e
Chris Toomey <chris@ctoomey.com>
2024-12-16 16:51:47 -0500

    Swap checkall for august raycast script

--- raycast/script-commands/august.sh
+++ raycast/script-commands/august.sh
@@ -2,18 +2,17 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Checkall
+# @raycast.title August
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 🔄
 
 # Documentation:
-# @raycast.description Check all work sources
+# @raycast.description August helper scripts
 # @raycast.author Chris Toomey
 # @raycast.authorURL https://ctoomey.com
 
-# open -a Terminal ~/code/things-report/bin/checkall
-
 open -a iTerm
-tmux display-popup -d "$HOME/code/things-report" -xC -yC -w80% -h80% "bin/checkall"
+
+tmux display-popup -d "$HOME/code/work/august/july" -xC -yC -w60% -h70% -EE "ls bin; zsh"
